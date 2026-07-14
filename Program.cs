a74eda80fd100d4b928b917c2a4fe162de6326df
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-14 13:48:10 +0300

    Finnished Task 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using CosmoAppAPI.Services.Abstractions;
+using CosmoAppAPI.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.Add(new ServiceDescriptor(typeof(IAPODServices), typeof(APODServices), ServiceLifetime.Singleton));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
